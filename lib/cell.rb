fdcc68af5915d269b39e940d6622c64fc9810ce3
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-18 13:49:49 -0600

    Correct place_ship method

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,26 +3,17 @@ require './lib/ship'
 class Cell
   attr_reader :coordinate
   attr_accessor :a_row, :b_row, :c_row, :d_row\
-  attr_accessor :coords
+  attr_accessor :coords, :ship_position
   def initialize(coordinate)
     @coordinate = coordinate
     @a_row = [".", ".", ".", "."]
     @b_row = [".", ".", ".", "."]
     @c_row = [".", ".", ".", "."]
     @d_row = [".", ".", ".", "."]
-    #@coords = []
   end
 
   def place_ship(ship)
-    puts "Where would you like to place your ship? Your ship requires #{ship.length} cells."
-    puts
-    puts "Choose #{ship.length} cells in order, vertically or horizontally, from A1-A4, B1-B4, C1-C4, D1-D4"
-    puts
-    puts "Enter your coordinates with a space in between"
-    @coords << gets.chomp
-    @coords.split(" ")
-    @coords.each do |coord|
-      
+    ship_position = @coordinate
   end
   def fired_upon?
     return true if ship.health < ship.length
@@ -30,5 +21,9 @@ class Cell
 
   def fire_upon
     ship.hit
+
+  def render
+    if fired_upon? == true
+
   end
 end
