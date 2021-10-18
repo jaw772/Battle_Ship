f71fd759927e68b078941988bd35d1f9fa8f5c04
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-18 12:48:49 -0600

    Create Rspec tests for ship.rb

---
+++ spec/ship_spec.rb
@@ -0,0 +1,35 @@
+require './lib/ship'
+
+RSpec.describe Ship do
+  it "exists" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser).to be_instance_of(Ship)
+  end
+
+  it "has a name and health" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.name).to eq "Cruiser"
+    expect(cruiser.health).to eq 3
+  end
+
+  it "is alive" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.sunk?).to be false
+  end
+
+  it "takes hits" do
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    expect(cruiser.health).to eq 2
+    cruiser.hit
+    expect(cruiser.health).to eq 1
+  end
+
+  it "can be sunk" do
+    cruiser = Ship.new("Cruiser", 3)
+    3.times do (cruiser.hit)
+    expect(cruiser.health).to eq 0
+    expect(cruiser.sunk?).to be true
+  end
+end
+end
