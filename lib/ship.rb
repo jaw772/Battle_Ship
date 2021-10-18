a07a81f06276a6f7f46d5e46f3ed9ce3335a5f73
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-18 12:50:11 -0600

    Create ship class

---
+++ lib/ship.rb
@@ -0,0 +1,26 @@
+class Ship
+  attr_reader :name, :ship_size
+  attr_accessor :life
+  def initialize(name, ship_size)
+    @name = name
+    @ship_size = ship_size
+    @life = ship_size
+  end
+
+  def length
+    @ship_size
+  end
+
+  def health
+    @life
+  end
+
+  def sunk?
+    return true if @life == 0
+  end
+
+  def hit
+    @life -= 1
+  end
+
+end
